8e8004566408cfad995dd0f8326d6392888efe47
Dana <danaz.radetskaya@gmail.com>
2022-05-05 19:02:09 +0300

    добавила редактирование учителя

--- src/main/java/com/example/project/controllers/TeacherController.java
+++ src/main/java/com/example/project/controllers/TeacherController.java
@@ -4,13 +4,12 @@ import com.example.project.entity.*;
 import com.example.project.repo.ScoreRepository;
 import com.example.project.repo.StudentCourseRepository;
 import com.example.project.repo.TeacherRepository;
+import com.example.project.repo.UsersRepository;
 import org.hibernate.mapping.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -22,6 +21,8 @@ public class TeacherController {
     public TeacherRepository teacherRepository;
     @Autowired
     public ScoreRepository scoreRepository;
+    @Autowired
+    public UsersRepository usersRepository;
 
     protected static Teacher staticTeacher;
     public static void setStaticTeacher(Teacher teacher){
@@ -95,5 +96,33 @@ public class TeacherController {
             }
         }
         return "redirect:/teacherMainPage";
+    }
+
+    @GetMapping("/teacher/teacherProfile/{id}")
+    public String studProfileGet(Model model, @PathVariable("id") int id){
+        model.addAttribute("staticTeacher", getStaticTeacher());
+        return "teacherProfile";
+    }
+    @PatchMapping("/teacher/teacherProfile/{id}")
+    public String studProfilePatch(Model model, @ModelAttribute("teach") Teacher teacher) {
+        var newObj1 = teacherRepository.findById(teacher.getId()).get();
+        newObj1.setName(teacher.getName());
+        newObj1.setLastName(teacher.getLastName());
+        newObj1.setExperience(teacher.getExperience());
+        newObj1.setAdditionalInfo(teacher.getAdditionalInfo());
+        teacherRepository.save(newObj1);
+
+        var newObj2 = usersRepository.findById(newObj1.getUsers().getId()).get();
+        newObj2.setLogin(teacher.getUsers().getLogin());
+        newObj2.setPassword(teacher.getUsers().getPassword());
+        usersRepository.save(newObj2);
+
+        staticTeacher.setName(newObj1.getName());
+        staticTeacher.setLastName(newObj1.getLastName());
+        staticTeacher.setExperience(newObj1.getExperience());
+        staticTeacher.setAdditionalInfo(newObj1.getAdditionalInfo());
+        staticTeacher.setUsers(newObj2);
+        model.addAttribute("staticTeacher", getStaticTeacher());
+        return "teacherMainPage";
     }
 }
